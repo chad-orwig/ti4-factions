4de8367eef76f753bb7e82adac04124354ff4e1a
Chad Orwig <chad.orwig@slalom.com>
2018-06-28 18:37:15 -0400

    catch errors

--- src/client/components/ScrollToTop.jsx
+++ src/client/components/ScrollToTop.jsx
@@ -1,18 +1,38 @@
-const {Component} = require('react');
+const React = require('react');
+const Component = React.Component;
 const {withRouter} = require('react-router-dom');
 const PropTypes = require('prop-types');
 
 class ScrollToTop extends Component {
+    constructor () {
+        super();
+        this.state ={};
+    }
     componentDidUpdate(prevProps) {
         if (this.props.location !== prevProps.location) {
             window.scrollTo(0, 0);
         }
     }
+    componentDidCatch () {
+        this.setState({error : true});
+    }
 
     render() {
+        if(this.state.error) {
+            return this.errorPage();
+        }
         return this.props.children;
     }
 
+    errorPage() {
+        return (
+            <div className="error-panel">
+                <h1>Woops...</h1>
+                <p>Something went wrong.</p>
+                <a className="btn btn-primary" href="/">Reset</a>
+            </div>
+        );
+    }
 }
 
 ScrollToTop.propTypes = {
