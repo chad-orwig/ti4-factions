8aa2ef346bdae68cb421ad8127f3770100d1a959
Chad Orwig <chad.orwig@slalom.com>
2019-02-04 19:16:24 -0500

    more typos and remove the donate button for printing

--- src/client/assets/factions/naalu.js
+++ src/client/assets/factions/naalu.js
@@ -35,7 +35,8 @@ module.exports = {
             requirements : ['G', 'B'],
             text : [
                 'Naalu Fighter (Combat: 7; Move 2)',
-                'This unit may move without being transported'
+                'This unit may move without being transported.',
+                'Each fighter in excess of your ships\' capacity counts as 1/2 of a ship against your fleet pool.'
             ]
         }
     ],
